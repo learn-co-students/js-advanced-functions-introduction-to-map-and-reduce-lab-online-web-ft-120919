dc410f8c944c4c8927e3e153ba09a367e80986e5
github username <meliajm@gmail.com>
2020-04-01 17:29:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+const mapToNegativize = function(arr) {
+    return arr.map( elem => -1* elem)
+} 
+
+const mapToNoChange = function(arr) {
+    return arr.map ( elem => elem)
+}
+
+const mapToDouble = function(arr) {
+    return arr.map ( elem => 2*elem)
+}
+
+const mapToSquare = function(arr) {
+    return arr.map ( elem => elem**2)
+}
+
+const reduceToTotal = function(arr, startingValue) {
+    if (!!startingValue) {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return arr.reduce(reducer) + startingValue
+    } else {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return arr.reduce(reducer)
+    }
+}
+
+const reduceToAllTrue = function(arr) {
+    const isTrue = (elem) => !!elem
+    return arr.every(isTrue)
+}
+
+const reduceToAnyTrue = function(arr) {
+    const isTrue = (elem) => !!elem 
+    return arr.find(isTrue) ? true : false
+}
+
