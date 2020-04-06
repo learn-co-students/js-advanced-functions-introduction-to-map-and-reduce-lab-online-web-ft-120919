2d62598beb20b777238af157d8094700a898e92e
ben20262 <ben20262@aim.com>
2020-04-06 16:36:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function mapToNegativize(array) {
+    return array.map(value => {
+        return value * -1
+    })
+}
+
+function mapToNoChange(array) {
+    return array.map(value => {
+        return value
+    })
+}
+
+function mapToDouble(array) {
+    return array.map(value => {
+        return value * 2
+    })
+}
+
+function mapToSquare(array) {
+    return array.map(value => {
+        return value ** 2
+    })
+}
+
+function reduceToTotal(array, start=0) {
+    return start + array.reduce((total, value) => {
+        return total + value
+    })
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((bool=true, value) => {
+        return bool && !!value
+    })
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((bool=false, value) => {
+        return bool || !!value
+    })
+}
